5d7edfc97bcad8c458a2270198ab5db20e7d9e93
Ezra Mabatu <ezramabatu@gmail.com>
2026-01-13 10:23:00 +0300

    Add matrix initialization function
    Added a function to initialize a 2x2 matrix to zero.

--- src/matrix_multiplication.cpp
+++ src/matrix_multiplication.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 
+void initMat(int inputMatrix[2][2]){
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++){
+			inputMatrix[i][j] = 0;
+		}
+	}
+}
 int main(){
+	int matA[2][2];
+	int matB[2][2];
+	
 }
