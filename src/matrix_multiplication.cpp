56c9b90012f783434e532c2727561d0e5d5a9760
Ezra Mabatu <ezramabatu@gmail.com>
2026-01-13 10:41:33 +0300

    Implement matrix printing functionality
    Added a function to print a 2x2 matrix and called it in main.

--- src/matrix_multiplication.cpp
+++ src/matrix_multiplication.cpp
@@ -8,6 +8,15 @@ void initializeMatrix(int inputMatrix[2][2]){
 	}
 }
 
+void printMatrix(int inputMatrix[2][2]){
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++){
+			printf("%d ", inputMatrix[i][j]);
+		}
+		printf("\n");
+	}	
+}
+
 int main(){
 	int matA[2][2];
 	int matB[2][2];
@@ -21,4 +30,15 @@ int main(){
 			std::cin >> matA[i][j];
 		}
 	}	
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++){
+			printf("Enter element [%d][%d] for matrix B: \n", i, j);
+			std::cin >> matB[i][j];
+		}
+	}		
+	
+	std::cout << "Matrix A is: \n" ;
+	printMatrix(matA);
+	std::cout << "Matrix B is: \n" ;
+	printMatrix(matB);
 }
