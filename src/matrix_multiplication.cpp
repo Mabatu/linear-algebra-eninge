ae7acb99802805cb518dfa81d6d2b9cc2c7e19d9
Ezra Mabatu <ezramabatu@gmail.com>
2026-01-14 22:07:00 +0300

    Initialize resultMatrix for matrix multiplication
    Added resultMatrix initialization for multiplication.

--- src/matrix_multiplication.cpp
+++ src/matrix_multiplication.cpp
@@ -20,9 +20,11 @@ void printMatrix(int inputMatrix[2][2]){
 int main(){
 	int matA[2][2];
 	int matB[2][2];
+	int resultMatrix[2][2];
 	
 	initializeMatrix(matA);
 	initializeMatrix(matB);
+	initializeMatrix(resultMatrix);
 
 	for(int i = 0; i < 2; i++){
 		for(int j = 0; j < 2; j++){
